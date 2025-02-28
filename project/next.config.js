7fb8b350b1ee4922e93676e586d7035ab2d80e51
kinley-codes <kinleypenjor513@gmail.com>
2025-02-28 15:27:41 +0600

    Update next.config.js
    d

--- project/next.config.js
+++ project/next.config.js
@@ -4,10 +4,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
 
   },
+  images: {
+    domains: ["images.unsplash.com", "img.clerk.com"], // Allow images from Unsplash and Clerk
+  },
   images: { unoptimized: true },
 };
 module.exports = {
   trailingSlash: true,
 };
+
 
 module.exports = nextConfig;
