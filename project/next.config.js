e52d77eb63f39e041b67459a941fa2f4b3376e17
kinley-codes <kinleypenjor513@gmail.com>
2025-02-28 17:58:26 +0600

    cc
    sw

--- project/next.config.js
+++ project/next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-
   },
-  images: {
-    domains: ["images.unsplash.com", "img.clerk.com"], // Allow images from Unsplash and Clerk
-  },
-  images: { unoptimized: true },
-};
-module.exports = {
-  trailingSlash: true,
-};
-
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
